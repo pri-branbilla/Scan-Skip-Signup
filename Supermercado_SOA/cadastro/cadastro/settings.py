1ba362773a9f1a9513beff862823a00f9daf36c9
Priscilla <pns.2712@gmail.com>
2016-09-28 20:20:58 -0300

    x

--- Supermercado_SOA/cadastro/cadastro/settings.py
+++ Supermercado_SOA/cadastro/cadastro/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import mongoengine
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
