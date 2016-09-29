39bb34a40d93ec7b512280ea4d0cc84508dff202
pns2712 <pns.2712@gmail.com>
2016-09-29 09:43:50 -0300

    inits adicionados

--- Supermercado_SOA/cadastro/cadastro/wsgi.py
+++ Supermercado_SOA/cadastro/cadastro/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cadastro.settings")
-
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'cadastro.settings'
 application = get_wsgi_application()
