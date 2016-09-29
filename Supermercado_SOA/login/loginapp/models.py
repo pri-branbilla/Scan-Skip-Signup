08db2409f75a5a965ada6b1176fd03569809217b
pns2712 <pns.2712@gmail.com>
2016-09-29 11:00:36 -0300

    connect

--- Supermercado_SOA/login/loginapp/models.py
+++ Supermercado_SOA/login/loginapp/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from mongoengine import connect
 
+connect(
+    name='test',
+    username='user',
+    password='12345',
+    host='mongodb://admin:qwerty@localhost/production'
+)
 # Create your models here.
