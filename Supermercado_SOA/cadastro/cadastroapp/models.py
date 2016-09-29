864ae725f26668c083ca421d6040eccca13a01de
pns2712 <pns.2712@gmail.com>
2016-09-29 11:56:25 -0300

    comentando connects

--- Supermercado_SOA/cadastro/cadastroapp/models.py
+++ Supermercado_SOA/cadastro/cadastroapp/models.py
@@ -3,11 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 from mongoengine import connect
 
-connect(
-    name='test',
-    username='user',
-    password='12345',
-    host='mongodb://admin:qwerty@localhost/production'
-)
+#connect(
+#    name='test',
+#     username='user',
+#     password='12345',
+#     host='mongodb://admin:qwerty@localhost/production'
+#)
 # Create your models here.
 
